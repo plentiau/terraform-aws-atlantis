c0ffdfc396151955655b877072c2f5f030fb8964
Chris Fowles <chris.fowles@plenti.com.au>
2021-06-18 21:23:44 +1000

    Release v2.43.0

--- modules/github-repository-webhook/variables.tf
+++ modules/github-repository-webhook/variables.tf
@@ -4,18 +4,6 @@ variable "create_github_repository_webhook" {
   default     = true
 }
 
-variable "github_token" {
-  description = "Github token to use when creating webhook"
-  type        = string
-  default     = ""
-}
-
-variable "github_organization" {
-  description = "Github organization to use when creating webhook"
-  type        = string
-  default     = ""
-}
-
 variable "atlantis_allowed_repo_names" {
   description = "List of names of repositories which belong to the organization specified in `github_organization`"
   type        = list(string)
