df486fd7c90295996c8e496d5f47281f31119334
Chris Fowles <chris.fowles@plenti.com.au>
2021-06-18 21:20:58 +1000

    remove provider from module for for_each use

--- modules/github-repository-webhook/main.tf
+++ modules/github-repository-webhook/main.tf
@@ -1,8 +1,3 @@
-provider "github" {
-  token        = var.github_token
-  organization = var.github_organization
-}
-
 resource "github_repository_webhook" "this" {
   count = var.create_github_repository_webhook ? length(var.atlantis_allowed_repo_names) : 0
 
